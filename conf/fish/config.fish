9ffbe021eb1fb6bf0967a5a62b8aedcd319ce999
Benedikt Geissler <benedikt@g5r.eu>
2017-12-01 02:30:45 +0100

    Modify vscode keybinding (reset zoom) and add fish plugins

--- conf/fish/config.fish
+++ conf/fish/config.fish
@@ -35,4 +35,7 @@ alias g 'git'
 
 fundle plugin 'tuvistavie/fish-completion-helpers'
 fundle plugin 'edc/bass'
+fundle plugin 'fisherman/done'
+fundle plugin 'fisherman/humanize_duration'
+fundle plugin 'fisherman/docker-completion'
 fundle init
