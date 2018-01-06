796355a28d004c25427898c38499b19de68a87c5
Benedikt Geissler <benedikt@g5r.eu>
2018-01-06 01:09:21 +0100

    Color hostname in prompt when using SSH

--- conf/fish/functions/fish_prompt.fish
+++ conf/fish/functions/fish_prompt.fish
@@ -1,6 +1,28 @@
 function fish_prompt
     set last_status $status
-    printf '[%s@%s:%s%s%s] ' (whoami) (hostname) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+
+    echo -n '['
+
+    if test $USER = root
+        set_color red
+    end
+
+    echo -n $USER
+    set_color normal
+
+    if set -qx SSH_CONNECTION
+        if not set -qg HOST_COLOR
+            set -g HOST_COLOR (echo -n $HOSTNAME | sha256sum | head -c 6)
+        end
+        set_color $HOST_COLOR
+        echo -n "@$HOSTNAME"
+    end
+
+    echo -n ':'
+    set_color $fish_color_cwd
+    echo -n (prompt_pwd)
+    set_color normal
+    echo -n '] '
 
     # Sets the color of the last part of the prompt
     # according to the status of the last command
