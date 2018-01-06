47b3e21eb27c2b6db772577d05ef96b4682e56d4
Benedikt Geissler <benedikt@g5r.eu>
2018-01-06 21:35:21 +0100

    Add lskern function

---
+++ conf/fish/functions/lskern.fish
@@ -0,0 +1,6 @@
+function lskern --description 'Shows running and installed kernels'
+	set_color bryellow
+    uname -r | cut -d'-' -f-2
+    set_color normal
+    rpm -q kernel-default | cut -d'-' -f3- | cut -d'.' -f-4
+end
