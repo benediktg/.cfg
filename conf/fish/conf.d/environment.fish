8b364195059e5abc036359dd2eab28888f14b7b6
Benedikt Geissler <benedikt@g5r.eu>
2018-01-06 03:51:16 +0100

    Fix PATH

--- conf/fish/conf.d/environment.fish
+++ conf/fish/conf.d/environment.fish
@@ -2,6 +2,12 @@ set -x EDITOR vim
 set -x BROWSER firefox
 set -x GCC_COLORS 'error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 set -x GOPATH $HOME/.local/go
+if test -d $HOME/bin
+    set -x PATH (string match -v $HOME/bin $PATH) $HOME/bin
+end
+if test -d $HOME/.local/bin
+    set -x PATH (string match -v $HOME/.local/bin $PATH) $HOME/.local/bin
+end
 if test -d $GOPATH/bin
     set -x PATH (string match -v $GOPATH/bin $PATH) $GOPATH/bin
 end
--- conf/fish/functions/setup_universal_variables.fish
+++ conf/fish/functions/setup_universal_variables.fish
@@ -1,4 +1,3 @@
 function setup_universal_variables
-    set -U fish_user_paths $HOME/bin $HOME/.local/bin
     set -U fish_prompt_pwd_dir_length 2
 end
