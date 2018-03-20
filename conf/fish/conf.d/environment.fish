bdf6afa21255e8f287efe458e7ed6a08abde4840
Benedikt Geissler <benedikt@g5r.eu>
2018-03-20 18:43:42 +0100

    Update split22to1

--- conf/fish/conf.d/environment.fish
+++ conf/fish/conf.d/environment.fish
@@ -2,6 +2,9 @@ set -x EDITOR vim
 set -x BROWSER firefox
 set -x GCC_COLORS 'error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 set -x GOPATH $HOME/.local/go
+if test -d $HOME/.dotfiles/bin
+    set -x PATH (string match -v $HOME/.dotfiles/bin $PATH) $HOME/.dotfiles/bin
+end
 if test -d $HOME/bin
     set -x PATH (string match -v $HOME/bin $PATH) $HOME/bin
 end
