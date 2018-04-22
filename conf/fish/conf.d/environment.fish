652cd83a68e30e44c6bf297738c832120a5c2b4d
Benedikt Geissler <benedikt@g5r.eu>
2018-04-22 03:11:43 +0200

    Clean up fish PATH setup

--- conf/fish/conf.d/environment.fish
+++ conf/fish/conf.d/environment.fish
@@ -2,23 +2,18 @@ set -x EDITOR vim
 set -x BROWSER firefox
 set -x GCC_COLORS 'error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 set -x GOPATH $HOME/.local/go
-if test -d $HOME/.dotfiles/bin
-    set -x PATH (string match -v $HOME/.dotfiles/bin $PATH) $HOME/.dotfiles/bin
-end
-if test -d $HOME/bin
-    set -x PATH (string match -v $HOME/bin $PATH) $HOME/bin
-end
-if test -d $HOME/.local/bin
-    set -x PATH (string match -v $HOME/.local/bin $PATH) $HOME/.local/bin
-end
-if test -d $GOPATH/bin
-    set -x PATH (string match -v $GOPATH/bin $PATH) $GOPATH/bin
-end
-if test -d $HOME/.cargo/bin
-    set -x PATH (string match -v $HOME/.cargo/bin $PATH) $HOME/.cargo/bin
-end
-if test -d $HOME/.linuxbrew/bin
-    set -x PATH (string match -v $HOME/.linuxbrew/bin $PATH) $HOME/.linuxbrew/bin
+
+set -l _path_dirs \
+    $HOME/.dotfiles/bin \
+    $HOME/bin \
+    $HOME/.local/bin \
+    $GOPATH/bin \
+    $HOME/.cargo/bin \
+    $HOME/.linuxbrew/bin
+for d in $_path_dirs
+    if test -d $d; and not contains $d $PATH
+        set -x PATH $PATH $d
+    end
 end
 
 setup_universal_variables
