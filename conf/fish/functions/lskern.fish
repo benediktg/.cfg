5bcb22e295e0b4b2f97176550e2e4de77131dfee
Benedikt Geißler <benedikt@g5r.eu>
2019-01-20 21:47:03 +0100

    Fix lskern

--- conf/fish/functions/lskern.fish
+++ conf/fish/functions/lskern.fish
@@ -1,6 +1,6 @@
 function lskern --description 'Shows running and installed kernels'
-    set_color bryellow
+    set_color yellow
     uname -r | cut -d'-' -f-2
     set_color normal
-    rpm -q kernel-default | cut -d'-' -f3- | cut -d'.' -f-4
+    rpm -q kernel-default | cut -d'-' -f3- | rev | cut -d'.' -f2- | rev | sort -r
 end
