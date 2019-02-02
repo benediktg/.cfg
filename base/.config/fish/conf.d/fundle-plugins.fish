8c649a55b794a126848588cace72add2321c091c
Benedikt Geißler <benedikt@g5r.eu>
2019-02-02 07:12:53 +0100

    Remove unavailable fish bundles

--- base/.config/fish/conf.d/fundle-plugins.fish
+++ base/.config/fish/conf.d/fundle-plugins.fish
@@ -1,9 +1,4 @@
 fundle plugin 'tuvistavie/fish-completion-helpers'
 fundle plugin 'edc/bass'
-fundle plugin 'fisherman/done'
-fundle plugin 'fisherman/humanize_duration'
-fundle plugin 'fisherman/docker-completion'
-fundle plugin 'fisherman/z'
-fundle plugin 'fisherman/getopts'
 
 fundle init
