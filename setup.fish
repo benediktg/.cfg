e4e8f27a1e56ad2a63b03534f9063e41a45ce6a0
Benedikt Geißler <benedikt@g5r.eu>
2019-02-02 06:58:46 +0100

    Add initialisation command to setup.fish

--- setup.fish
+++ setup.fish
@@ -1,6 +1,8 @@
 #!/usr/bin/fish
 
+stow -t ~ base bin vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl -fLo ~/.config/fish/functions/fundle.fish --create-dirs https://git.io/fundle
 curl -fLo ~/.local/bin/diff-so-fancy https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy
 chmod +x ~/.local/bin/diff-so-fancy
+vim +PlugUpdate +qa
