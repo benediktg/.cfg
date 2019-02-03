cad3c25963193e0aa65c0cd35fb9b58c71e580df
Benedikt Geißler <benedikt@g5r.eu>
2019-02-03 12:57:22 +0100

    Quickfix

--- fish/.config/fish/functions/is_ssh.fish
+++ fish/.config/fish/functions/is_ssh.fish
@@ -5,11 +5,11 @@ function is_ssh
         set p $fish_pid
     end
     cat /proc/$p/stat | read pid name x ppid y
-    if string match -qr 'sshd' $name
+    if string match -qr 'sshd' "$name"
         echo "Is SSH: $pid $name"
         return 0
     end
-    if test $ppid -le 1
+    if test "$ppid" -le 1
         echo "Adam is $pid $name"
         return 1
     end
