c812dafb6d83a0d7c8a66d052553a936e2f01ee1
Benedikt Geißler <benedikt@g5r.eu>
2019-05-23 17:32:08 +0200

    Fix zypper functions

--- fish/.config/fish/functions/zypp-mark-auto.fish
+++ fish/.config/fish/functions/zypp-mark-auto.fish
@@ -1,6 +1,5 @@
 function zypp-mark-auto
-	for package in $argv
-sudo sed -i "\$ a $package" /var/lib/zypp/AutoInstalled
-end
-echo $argv | xargs sudo zypper in -f
+    for package in $argv
+        sudo sed -i "\$ a $package" /var/lib/zypp/AutoInstalled
+    end
 end
--- fish/.config/fish/functions/zypp-mark-manual.fish
+++ fish/.config/fish/functions/zypp-mark-manual.fish
@@ -1,5 +1,5 @@
 function zypp-mark-manual
 	for line in $argv
-sudo sed -i "/^$line\$/d" /var/lib/zypp/AutoInstalled
-end
+        sudo sed -i "/^$line\$/d" /var/lib/zypp/AutoInstalled
+    end
 end
