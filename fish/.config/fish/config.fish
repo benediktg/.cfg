fec759e9950487ff3d160038fb478318a6cbfc9d
Benedikt Geißler <benedikt@g5r.eu>
2019-06-24 19:08:56 +0200

    Add fish abbr, fix vim mappings

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,6 +3,7 @@ if status is-interactive
     abbr -ag cdt 'cd (mktemp -d)'
     abbr -ag dc cd
     abbr -ag kin 'kinit geib -R; or kinit geib'
+    abbr -ag nvi nvim
     abbr -ag syc 'systemctl'
     abbr -ag sycu 'systemctl --user'
     abbr -ag zps 'sudo zypper ps -s'
