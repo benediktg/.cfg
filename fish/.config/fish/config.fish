c0989a899a3b6f97cf287be9c7b58c327b5cac5a
Benedikt Geißler <benedikt@g5r.eu>
2019-07-02 15:34:25 +0200

    Auto-install fundle

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,6 +4,13 @@ if is_ssh >/dev/null
 end
 # }}}1
 # fundle plugins {{{1
+# fundle auto-install {{{
+if not functions -q fundle
+    curl -sfLo ~/.config/fish/functions/fundle.fish --create-dirs https://raw.githubusercontent.com/tuvistavie/fundle/master/functions/fundle.fish
+    and fish -c 'fundle install'
+    and exec fish
+end
+# }}}
 fundle plugin 'tuvistavie/fish-completion-helpers'
 fundle plugin 'edc/bass'
 fundle init
--- fish/.config/fish/functions/install-fundle.fish
+++
@@ -1,8 +0,0 @@
-function install-fundle
-    if test -f ~/.config/fish/functions/fundle.fish
-        fundle self-update
-    else
-        curl -sfLo ~/.config/fish/functions/fundle.fish --create-dirs https://git.io/fundle
-        fundle install
-    end
-end
