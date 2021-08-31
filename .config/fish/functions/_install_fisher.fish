d52cf56e2fd5a2ab59db8a7d746dbeddd91294f5
Benedikt Geißler <mail@benediktgeissler.de>
2021-08-31 19:46:46 +0200

    Update fisher install script

--- .config/fish/functions/_install_fisher.fish
+++ .config/fish/functions/_install_fisher.fish
@@ -2,4 +2,6 @@ function _install_fisher
     set -U fisher_path "~/.fisher"
     test -d $fisher_path || mkdir $fisher_path
     curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+    config checkout -- ~/.config/fish/fish_plugins
+    fisher update
 end
