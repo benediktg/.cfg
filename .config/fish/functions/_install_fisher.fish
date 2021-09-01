dfe29eb1c879cd6eb1d5b64812fed5b6ffe18d7f
Benedikt Geißler <mail@benediktgeissler.de>
2021-09-01 13:24:53 +0200

    Fix fisher install script

--- .config/fish/functions/_install_fisher.fish
+++ .config/fish/functions/_install_fisher.fish
@@ -1,5 +1,5 @@
 function _install_fisher
-    set -U fisher_path "~/.fisher"
+    set -U fisher_path "$HOME/.fisher"
     test -d $fisher_path || mkdir $fisher_path
     curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
     config checkout -- ~/.config/fish/fish_plugins
